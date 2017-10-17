1e0d4168d4f5bc64916dbb0e41adf1428b2bc52b
peterjames <joyukr>
2017-10-17 21:45:03 +0300

    Initial commit

---
+++ src/com/company/Service.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Service {
+
+    public static void main(String[] args) {
+        final Service service = new Service();
+        final int age = 22;
+        final String name = "Alex";
+        final User user = service.createUser(name, age);
+        service.print(user);
+    }
+
+    private User createUser(String name, int age) {
+        final User user = new User();
+        user.setAge(age);
+        user.setName(name);
+        return user;
+    }
+
+    private void print(User user) {
+        System.out.println(user.getAge());
+        System.out.println(user.getName());
+    }
+
+
+}
