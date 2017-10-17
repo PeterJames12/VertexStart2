0fd87db4fd3a74e350bca0388af7a6fcd2ad8aaf
peterjames <joyukr>
2017-10-17 21:56:25 +0300

    added new method and java doc

--- src/com/vartex/company/Service.java
+++ src/com/vartex/company/Service.java
@@ -7,9 +7,13 @@ public class Service {
         final int age = 22;
         final String name = "Alex";
         final User user = service.createUser(name, age);
+        service.something();
         service.print(user);
     }
 
+    /**
+     * Create user with given name and age.
+     */
     private User createUser(String name, int age) {
         final User user = new User();
         user.setAge(age);
@@ -17,10 +21,18 @@ public class Service {
         return user;
     }
 
+    /**
+     * Just print user.
+     */
     private void print(User user) {
         System.out.println(user.getAge());
         System.out.println(user.getName());
     }
 
-
+    /**
+     * Print message after user was created.
+     */
+    private void something() {
+        System.out.println("User created");
+    }
 }
