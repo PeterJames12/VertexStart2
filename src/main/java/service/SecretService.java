155ae429065747140e7e3015339c47b087f8a767
Vasya L <Lisovyi_V_V@ukr.net>
2017-10-19 21:44:07 +0300

    added 3 methods

--- src/main/java/service/SecretService.java
+++ src/main/java/service/SecretService.java
@@ -4,20 +4,27 @@ package service;
  * @author Igor Hnes on 10/19/17.
  */
 public class SecretService {
+    public static void main(String[] args) {
 
-    public void doSomething() {
+    }
+    public void met1 (){
+        System.out.println("met1");
+    }
+    public void met2(){
+        System.out.println("met2");
+
+    }
+    public void met3(){
+        System.out.println("met3");
+
+    }
+    private void met4(){
+
+    }
+    private void met5(){
 
     }
 
-    public void say() {
 
-    }
 
-    private void run() {
-
-    }
-
-    private void go() {
-
-    }
 }
